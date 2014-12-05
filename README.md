a8ff243c81e96a3190941208c03c3aae3219f057
Warren Mira <warrenmira@gmail.com>
2014-12-06 00:14:37 +0800

    updated api and README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 dropkit
 =======
+
+dropkit is a node.js module for Digital Ocean's REST API.
+
+Usage:
+
+ npm install --save dropkit
+
+```javascript
+ var dropkit = require("dropkit");
+ var do = new dropkit("TOKEN");
+
+ //DropKit uses BlueBird and returns promises instead of callbacks.
+
+ do.accounts().then(function(account) {
+    console.log(account);
+ }).error(function(error) {
+    console.log("something bad happened");
+ });
+```
+
+API:
+
+ do.accounts();
+
+ do.domains();
+ do.domain(domainName);
+ do.domain().delete(domainName);
+ do.domain().create({'name' : 'name.com','ip_address' : '127.0.0.1'});
--- index.js
+++ index.js
@@ -7,20 +7,77 @@ var Promise = require("bluebird");
 var https = require("https");
 var util = require("util");
 
+
+
+/**
+ * augments the request option with the baseOption
+ *
+ */
 var HttpOptionCreator = function(baseOption) {
 
-    /**
-     * augments the request option with the baseOption
-     *
-     */
     return function(requestOption) {
-        return util._extend(baseOption,requestOption);
-    };
+        return util._extend(baseOption, requestOption);
+    }
 }
 
 
 var toJSON = function(buffer) {
   return JSON.parse(buffer.toString('utf-8'));
+};
+
+var createPromise = function(params,towrite) {
+
+    return new Promise(function (resolve, reject) {
+        var req = https.request(params,function(res) {
+            var buffer = "";
+            //204 is successful delete request
+            if ( res.statusCode === 200 || res.statusCode === 204 ) {
+                res.on('data', function (chunk) {
+                    buffer += chunk;
+                });
+            } else {
+                reject(new Error("Error: Non OK Status Code: " + res.statusCode));
+            }
+            res.on('error',function(err) {
+                reject(err);
+            });
+
+            res.on('end',function() {
+                resolve(toJSON(buffer));
+            });
+        });
+
+        if ( towrite ) {
+            req.write(towrite);
+        }
+
+        //flush it
+        req.end();
+
+        req.on('error', function(e) {
+            reject(e);
+        });
+    });
+};
+
+var domain = function(DropKit) {
+    this.dropkit = DropKit;
+
+    return this;
+};
+
+domain.prototype.create = function(name,ipaddress) {
+    var tosubmit = {
+        name : name,
+        ip_address : ipaddress
+    };
+
+    return createPromise(this.dropkit.createOption({ method: 'POST' , path: '/v2/domains'}),JSON.stringify(tosubmit));
+
+};
+
+domain.prototype.delete = function(name) {
+    return createPromise(this.dropkit.createOption({ method: 'DELETE' , path: '/v2/domains/' + name}));
 };
 
 var DropKit = function(token) {
@@ -33,33 +90,46 @@ var DropKit = function(token) {
             'Authorization' : 'Bearer ' + token
         }
     };
-
-    var createOption = HttpOptionCreator(baseOption);
-
-    this.account = function() {
-        return new Promise(function (resolve, reject) {
-            var req = https.request(createOption({ method: 'GET' , path: '/v2/account'}),function(res) {
-                res.on('data',function(data) {
-                    var result = toJSON(data);
-                    resolve(result);
-                });
-            });
-            req.end();
-        });
-
-
-    };
+    this.createOption = HttpOptionCreator(baseOption);
 };
 
+/**
+ * Return account info
+ *
+ * https://developers.digitalocean.com/#get-user-information
+ *
+ */
+DropKit.prototype.accounts = function() {
+    return createPromise(this.createOption({ method: 'GET' , path: '/v2/account'}));
+};
+
+/**
+ * Domains
+ *
+ * https://developers.digitalocean.com/#list-all-domains
+ *
+ */
+DropKit.prototype.domains = function() {
+    return createPromise(this.createOption({ method: 'GET' , path: '/v2/domains'}));
+};
 
 
 /**
- * Create a digital ocean droplet instance
+ * https://developers.digitalocean.com/#retrieve-an-existing-domain
+ * https://developers.digitalocean.com/#create-a-new-domain
  *
- * @param token
+ *
+ * @param domainName
+ * @returns {domain}
  */
-DropKit.create = function(token) {
-    return new DropKit(token);
+DropKit.prototype.domain = function(domainName) {
+    if ( domainName ) {
+        return createPromise(this.createOption({ method: 'GET' , path: '/v2/domains/' + domainName}))
+    } else {
+        return new domain(this);
+    }
 };
+
+
 
 module.exports = DropKit;
